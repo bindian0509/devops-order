d3b1b9fdadd182ebb972a0d0745b88a06257da27
Bharat V <bindian0509@gmail.com>
2023-11-22 23:59:59 +0530

    Adding Order Service Mapper - 2

--- src/main/java/com/bharat/devops/order/mapper/OrderMapper.java
+++ src/main/java/com/bharat/devops/order/mapper/OrderMapper.java
@@ -2,6 +2,8 @@ package com.bharat.devops.order.mapper;
 
 import com.bharat.devops.order.dto.OrderDTO;
 import com.bharat.devops.order.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 /*
  * @author Bharat V. <bindian0509@gmail.com>
